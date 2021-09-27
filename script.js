fd5562a5e5f2cabe03c7a6be0daca5d32e0613af
Brij Kishor <exreply7878@gmail.com>
2021-09-27 11:45:56 +0530

    Added Sidebar Feature

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    //  do nothing
+    // sidebar
+    let closeButtonSidebar = document.querySelector('.close-sidebar');
+    closeButtonSidebar.addEventListener("click", ()=>{
+        let sidebar = document.querySelector('.sidebar-background');
+        sidebar.style.width = "0%";
+    });
+    //sidebar button
+    let sidebarButton = document.querySelector(".sidebar-button");
+    sidebarButton.addEventListener("mouseover", ()=>{
+        sidebarButton.style.animationName = "uptodown";
+    });
+    sidebarButton.addEventListener("mouseout", ()=>{
+        sidebarButton.style.animationName = "";
+    });
+    sidebarButton.addEventListener("click", ()=>{
+        let sidebar = document.querySelector('.sidebar-background');
+        sidebar.style.backdropFilter = "blur(0px)";
+        sidebar.style.backgrounColor = "transparent";
+        sidebar.style.width = "100%";
+    })
 });
